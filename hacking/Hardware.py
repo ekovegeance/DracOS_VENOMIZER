ed928b2678c4ac4df89b6345aa4742bea3e4525d
faizH3 <201420026@student.binadarma.ac.id>
2022-03-02 02:23:38 -0500

    update tools

--- hacking/Hardware.py
+++ hacking/Hardware.py
@@ -22,28 +22,17 @@ def Hardware():
         )
     )
     print(R("    00. exit"))
-    menu = input(G("[") + R("DracOS") + G("]select>"))
-    if menu == "1":
+
+    lists = ('android-sdk', 'apktool', 'arduino', 'dex2jar', 'sakis3g', 'smali')
+
+    menu = int(input(G("[") + R("DracOS") + G("]select>")))
+    if menu:
+        menu -= 1
         # Call function
-        Android_sdk()
-    elif menu == "2":
-        # Call function
-        Apktool()
-    elif menu == "3":
-        # Call function
-        Arduino()
-    elif menu == "4":
-        # Call function
-        Dex2jar()
-    elif menu == "5":
-        # Call function
-        Sakis3G()
-    elif menu == "6":
-        # Call function
-        Smalis()
-    elif menu == "0":
+        hardware_tool(lists[menu])
+    elif menu == 0:
         os.system("python3 /usr/bin/DracOS_VENOMIZER/venomizer.py")  # //usr/bin/
-    elif menu == "00":
+    elif menu == 00:
         exit()
     else:
         print(R('Wrong Input!'))
@@ -56,130 +45,22 @@ def back():
     Hardware()
 
 #Function
-#Android-sdk
-def Android_sdk():
-    if os.path.isfile("/usr/bin/android-sdk"):
+#hardware tool
+def hardware_tool(a):
+    if os.path.isfile(f"/usr/bin/{a}"):
         os.system("clear")
         print(B("Tools Available"))
         # input()
         # back()
     else:
         os.system(
-            'xterm -T "☣ INSTALL Android-sdk ☣" -geometry 100x30 -e "sudo apt install android-sdk"'
+            f'xterm -T "☣ INSTALL {a} ☣" -geometry 100x30 -e "sudo apt install {a}"'
         )
         os.system("clear")
-        if os.path.isfile("/usr/bin/android-sdk"):
-            print(B("Android-sdk Already Installed"))
+        if os.path.isfile(f"/usr/bin/{a}"):
+            print(B(f"{a} Already Installed"))
         else:
-            print(R("Android-sdk Not Installed"))
+            print(R(f"{a} Not Installed"))
     input()
     back()
-        # end android-sdk
-
-#Apktool
-def Apktool():
-    if os.path.isfile("/usr/bin/apktool"):
-        os.system("clear")
-        print(B("Tools Available"))
-        # input()
-        # back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Apktool ☣" -geometry 100x30 -e "sudo apt install apktool"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/apktool"):
-            print(B("Apktool Already Installed"))
-        else:
-            print(R("Apktool Not Installed"))
-    input()
-    back()
-    # end apktool
-
-#Arduino
-def Arduino():
-    if os.path.isfile("/usr/bin/arduino"):
-        os.system("clear")
-        print(B("Tools Available"))
-        # input()
-        # back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Arduino ☣" -geometry 100x30 -e "sudo apt install arduino"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/arduino"):
-            print(B("Arduino Already Installed"))
-        else:
-            print(R("Arduino Not Installed"))
-    input()
-    back()
-    # end arduino
-
-#Dex2jar
-def Dex2jar():
-    if os.path.isfile("/usr/bin/dex2jar"):
-        os.system("clear")
-        print(B("Tools Available"))
-        # input()
-        # back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Dex2jar ☣" -geometry 100x30 -e "sudo apt install dex2jar"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/dex2jar"):
-            print(B("Dex2jar Already Installed"))
-        else:
-            print(R("Dex2jar Not Installed"))
-    input()
-    back()
-    # end dex2jar
-
-#Sakis3G
-def Sakis3G():
-    if os.path.isfile("/usr/bin/sakis3g"):
-        os.system("clear")
-        print(B("Tools Available"))
-        # input()
-        # back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Sakis3G ☣" -geometry 100x30 -e "sudo apt install sakis3g"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/sakis3g"):
-            print(B("Sakis3G Already Installed"))
-        else:
-            print(R("Sakis3G Not Installed"))
-    input()
-    back()
-    # end sakis3g
-
-#Smalis
-def Smalis():
-    if os.path.isfile("/usr/bin/smali"):
-        os.system("clear")
-        print(B("Tools Available"))
-        # input()
-        # back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Smalis ☣" -geometry 100x30 -e "sudo apt install smali"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/smali"):
-            print(B("Smalis Already Installed"))
-        else:
-            print(R("Smalis Not Installed"))
-    input()
-    back()
-    # end smalis
-
-
-
-
-
-# Looping
-# while True:
-#     Hardware()
+        # end hardware_tool 
--- hacking/Maintaining.py
+++ hacking/Maintaining.py
@@ -1,6 +1,7 @@
 #! /usr/share/bin/python3
 #! -*- coding: utf-8 -*-
 #! PasswordAttack.py
+from json.tool import main
 import os
 from color import *
 import Logo
@@ -33,61 +34,16 @@ def MaintiningF():
         )
     )
     print(R("    00. exit"))
-    menu = input(G("[") + R("DracOS") + G("]select>"))
-    if menu == "1":
+    lists = ('cryptcat', 'cymothoa', 'dbd', 'dns2tcp', 'http-tunnel', 'intersect', 'nishang',
+     'polenum', 'powersploit', 'pwnat', 'ridenum', 'sbd', 'shellter', 'u3-pwn', 'webshells', 'weevely', 'winexe')
+    menu = int(input(G("[") + R("DracOS") + G("]select>")))
+    if menu:
+        menu -= 1
         # Call function
-        Cryptcat()
-    elif menu == "2":
-        # Call function
-        Cymothoa()
-    elif menu == "3":
-        # Call function
-        Dbd()
-    elif menu == "4":
-        # Call function
-        Dns2tcp()
-    elif menu == "5":
-        # Call function
-        HTTPTunnel()
-    elif menu == "6":
-        # Call function
-        Intersect()
-    elif menu == "7":
-        # Call function
-        Nishang()
-    elif menu == "8":
-        # Call function
-        Polenum()
-    elif menu == "9":
-        # Call function
-        PowerSploit()
-    elif menu == "10":
-        # Call function
-        Pwnat()
-    elif menu == "11":
-        # Call function
-        RidEnum()
-    elif menu == "12":
-        # Call function
-        Sbd()
-    elif menu == "13":
-        # Call function
-        Shellter()
-    elif menu == "14":
-        # Call function
-        U3Pwn()
-    elif menu == "15":
-        # Call function
-        Webshells()
-    elif menu == "16":
-        # Call function
-        Weevely()
-    elif menu == "17":
-        # Call function
-        Winexe()
-    elif menu == "0":
+        maintaining_tool(lists[menu])
+    elif menu == 0:
         os.system("python3 /usr/bin/DracOS_VENOMIZER/venomizer.py")  # //usr/bin/
-    elif menu == "00":
+    elif menu == 00:
         exit()
     else:
         print(R('Wrong Input!'))
@@ -100,349 +56,22 @@ def back():
     MaintiningF()
 
 #Function
-#Cryptcat
-def Cryptcat():
-    if os.path.isfile("/usr/bin/cryptcat"):
+#maintaining_tool
+def maintaining_tool(a):
+    if os.path.isfile(f"/usr/bin/{a}"):
         os.system("clear")
         print(B("Tools Available"))
         input()
         back()
     else:
         os.system(
-            'xterm -T "☣ INSTALL Cryptcat ☣" -geometry 100x30 -e "sudo apt install cryptcat"'
+            f'xterm -T "☣ INSTALL {a} ☣" -geometry 100x30 -e "sudo apt install {a}"'
         )
         os.system("clear")
-        if os.path.isfile("/usr/bin/cryptcat"):
-            print(B("Cryptcat Already Installed"))
+        if os.path.isfile(f"/usr/bin/{a}"):
+            print(B(f"{a} Already Installed"))
         else:
-            print(R("Not Installing Cryptcat"))
+            print(R(f"Not Installing {a}"))
         input()
         back()
-        # end Cryptcat
-
-#Cymothoa
-def Cymothoa():
-    if os.path.isfile("/usr/bin/cymothoa"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Cymothoa ☣" -geometry 100x30 -e "sudo apt install cymothoa"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/cymothoa"):
-            print(B("Cymothoa Already Installed"))
-        else:
-            print(R("Not Installing Cymothoa"))
-        input()
-        back()
-    # end Cymothoa
-
-#Dbd
-def Dbd():
-    if os.path.isfile("/usr/bin/dbd"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Dbd ☣" -geometry 100x30 -e "sudo apt install dbd"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/dbd"):
-            print(B("Dbd Already Installed"))
-        else:
-            print(R("Not Installing Dbd"))
-        input()
-        back()
-    # end Dbd
-
-#Dns2tcp
-def Dns2tcp():
-    if os.path.isfile("/usr/bin/dns2tcp"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Dns2tcp ☣" -geometry 100x30 -e "sudo apt install dns2tcp"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/dns2tcp"):
-            print(B("Dns2tcp Already Installed"))
-        else:    
-            print(R("Not Installing Dns2tcp"))
-        input()
-        back()
-    # end Dns2tcp
-    
-#HTTPTunnel
-def HTTPTunnel():
-    if os.path.isfile("/usr/bin/http-tunnel"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL HTTPTunnel ☣" -geometry 100x30 -e "sudo apt install http-tunnel"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/http-tunnel"):
-            print(B("HTTPTunnel Already Installed"))
-        else:    
-            print(R("Not Installing HTTPTunnel"))
-        input()
-        back()
-    # end HTTPTunnel
-
-#Intersect
-def Intersect():
-    if os.path.isfile("/usr/bin/intersect"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Intersect ☣" -geometry 100x30 -e "sudo apt install intersect"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/intersect"):
-            print(B("Intersect Already Installed"))
-        else:    
-            print(R("Not Installing Intersect"))
-        input()
-        back()
-    # end Intersect
-
-#Nishang
-def Nishang():
-    if os.path.isfile("/usr/bin/nishang"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Nishang ☣" -geometry 100x30 -e "sudo apt install nishang"'
-        )
-        os.system("clear")
-        if os.path.isfile('/usr/bin/nishang'):
-            print(B("Nishang Already Installed"))
-        else:    
-            print(R("Not Installing Nishang"))
-        input()
-        back()
-    # end Nishang
-
-#Polenum
-def Polenum():
-    if os.path.isfile("/usr/bin/polenum"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Polenum ☣" -geometry 100x30 -e "sudo apt install polenum"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/polenum"):
-            print(B("Polenum Already Installed"))
-        else:    
-            print(R("Not Installing Polenum"))
-        input()
-        back()
-    # end Polenum
-
-#PowerSploit
-def PowerSploit():
-    if os.path.isfile("/usr/bin/powersploit"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL PowerSploit ☣" -geometry 100x30 -e "sudo apt install powersploit"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/powersploit"):
-            print(B("PowerSploit Already Installed"))
-        else:    
-            print(R("Not Installing PowerSploit"))
-        input()
-        back()
-    # end PowerSploit
-
-#Pwnat
-def Pwnat():
-    if os.path.isfile("/usr/bin/pwnat"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Pwnat ☣" -geometry 100x30 -e "sudo apt install pwnat"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/pwnat"):
-            print(B("Pwnat Already Installed"))
-        else:    
-            print(R("Not Installing Pwnat"))
-        input()
-        back()
-    # end Pwnat
-
-#RidEnum
-def RidEnum():
-    if os.path.isfile("/usr/bin/ridenum"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL RidEnum ☣" -geometry 100x30 -e "sudo apt install ridenum"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/ridenum"):
-            print(B("RidEnum Already Installed"))
-        else:    
-            print(R("Not Installing RidEnum"))
-        input()
-        back()
-    # end RidEnum
-
-#Sbd
-def Sbd():
-    if os.path.isfile("/usr/bin/sbd"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Sbd ☣" -geometry 100x30 -e "sudo apt install sbd"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/sbd"):
-            print(B("Sbd Already Installed"))
-        else:    
-            print(R("Not Installing Sbd"))
-        input()
-        back()
-    # end Sbd
-
-#Shellter
-def Shellter():
-    if os.path.isfile("/usr/bin/shellter"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Shellter ☣" -geometry 100x30 -e "sudo apt install shellter"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/shellter"):
-            print(B("Shellter Already Installed"))
-        else:    
-            print(R("Not Installing Shellter"))
-        input()
-        back()
-    # end Shellter
-
-#U3-Pwn
-def U3Pwn():
-    if os.path.isfile("/usr/bin/u3-pwn"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL U3-Pwn ☣" -geometry 100x30 -e "sudo apt install u3-pwn"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/u3-pwn"):
-            print(B("U3-Pwn Already Installed"))
-        else:    
-            print(R("Not Installing U3-Pwn"))
-        input()
-        back()
-    # end U3-Pwn
-
-#Webshells
-def Webshells():
-    if os.path.isfile("/usr/bin/webshells"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Webshells ☣" -geometry 100x30 -e "sudo apt install webshells"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/webshells"):
-            print(B("Webshells Already Installed"))
-        else:    
-            print(R("Not Installing Webshells"))
-        input()
-        back()
-    # end Webshells
-
-#Weevely
-def Weevely():
-    if os.path.isfile("/usr/bin/weevely"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Weevely ☣" -geometry 100x30 -e "sudo apt install weevely"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/weevely"):
-            print(B("Weevely Already Installed"))
-        else:    
-            print(R("Not Installing Weevely"))
-        input()
-        back()
-    # end Weevely
-
-#Winexe
-def Winexe():
-    if os.path.isfile("/usr/bin/winexe"):
-        os.system("clear")
-        print(B("Tools Available"))
-        input()
-        back()
-    else:
-        os.system(
-            'xterm -T "☣ INSTALL Winexe ☣" -geometry 100x30 -e "sudo apt install winexe"'
-        )
-        os.system("clear")
-        if os.path.isfile("/usr/bin/winexe"):
-            print(B("Winexe Already Installed"))
-        else:    
-            print(R("Not Installing Winexe"))
-        input()
-        back()
-    # end Winexe
-
-
-
-
-# Looping
-# while True:
-#     Maintining()
+        # end maintaining_tool
