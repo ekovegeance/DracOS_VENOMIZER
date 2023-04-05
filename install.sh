71d9dfe4359cc1a961bccfc329c63263066843cd
Eko <ekovegeance7@gmail.com>
2023-04-05 09:54:23 +0700

    add install Python pip3

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "setup venomizer"
 sudo apt update
 sudo apt install git
-sudo apt install python3 
+sudo apt install python3
+sudo apt install pip
 sudo pip3 install -r requirements.txt
 sudo python3 setup.py
